edb4e3e739d25434d9dc5d56646ba4e1d1a2d948
Aqil F <aqilfaizal2004@gmail.com>
2025-11-06 00:00:33 -0800

    idk how

--- algorithm-visualizer/app/page.tsx
+++ algorithm-visualizer/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import DataStructure from "./dataStructure";
 export default function Home() {
 
-  let numbers: number[] = [1, 2, 3, 4, 5];
+  const numbers = [1, 2, 3, 4, 5];
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         My Array:
-        <DataStructure array={numbers} />
+        <DataStructure array={numbers}/>
       </main>
     </div>
   );
